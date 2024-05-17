f552f26e8eabc6c69f4ef2eabc969c674914c340
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2024-05-17 19:04:06 -0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,19 @@
 #include <Arduino.h>
 
+#define TIME 250
+
 void setup()
 {
-  // initialize LED digital pin as an output.
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
-
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
   digitalWrite(RED_LED, HIGH);
   digitalWrite(GREEN_LED, HIGH);
-  // wait for a second
-  delay(500);
-  // turn the LED off by making the voltage LOW
+  delay(TIME);
   digitalWrite(RED_LED, LOW);
   digitalWrite(GREEN_LED, LOW);
-   // wait for a second
-  delay(500);
+  delay(TIME);
 }
